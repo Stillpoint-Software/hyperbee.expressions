41ca75ae475c8ae5a4fe5e877f65eb2aef7fb5aa
Matt Edwards <matthew.edwards@wagglebee.net>
2024-09-10 16:24:56 -0400

    First pass at removing name dependency for field/parameter mapping

--- src/Hyperbee.AsyncExpressions/ParameterMappingVisitor.cs
+++ src/Hyperbee.AsyncExpressions/ParameterMappingVisitor.cs
@@ -4,30 +4,29 @@ using System.Reflection.Emit;
 
 namespace Hyperbee.AsyncExpressions;
 
-// TODO: This should not use a dictionary, but a list/hash of node instances
 public class ParameterMappingVisitor( Expression instance, List<FieldBuilder> fields ) : ExpressionVisitor
 {
-    private readonly Dictionary<string, MemberExpression> _mappingCache = [];
+    private readonly Dictionary<Expression, MemberExpression> _mappingCache = [];
     private readonly string[] _fieldNames = fields.Select( x => x.Name ).ToArray();
+
     protected override Expression VisitParameter( ParameterExpression node )
     {
-        if ( node.Name == null )
-            return node;
-
-        if ( _mappingCache.TryGetValue( node.Name, out var fieldAccess ) )
+        if ( _mappingCache.TryGetValue( node, out var fieldAccess ) )
             return fieldAccess;
 
-        if ( !TryGetFieldInfo( node.Name, out var fieldInfo ) )
+        if ( !TryGetFieldInfo( node, out var fieldInfo ) )
             return node;
 
         var fieldExpression = Expression.Field( instance, fieldInfo );
-        _mappingCache.Add( node.Name, fieldExpression );
+        _mappingCache.Add( node, fieldExpression );
 
         return fieldExpression;
 
-        bool TryGetFieldInfo( string name, out FieldInfo field )
+        bool TryGetFieldInfo( ParameterExpression parameterExpression, out FieldInfo field )
         {
-            var builderField = fields.FirstOrDefault( f => f.Name == $"_{name}" );
+            var name = $"_{parameterExpression.Name ?? parameterExpression.ToString()}";
+
+            var builderField = fields.FirstOrDefault( f => f.Name == name );
             if ( builderField != null )
             {
                 field = instance.Type.GetField( builderField.Name, BindingFlags.Instance | BindingFlags.Public )!;
@@ -43,8 +42,8 @@ public class ParameterMappingVisitor( Expression instance, List<FieldBuilder> fi
     {
         // TODO: Review with BF
         // Update each expression in a block to use only state machine fields/variables
-        return node.Update( 
-            node.Variables.Where( v => !_fieldNames.Contains(v.Name) ), 
+        return node.Update(
+            node.Variables.Where( v => !_fieldNames.Contains( v.Name ) ),
             node.Expressions.Select( Visit ) );
     }
 }
