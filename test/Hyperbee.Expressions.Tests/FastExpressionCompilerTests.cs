063df0c1441b1e565b71f36a0384d01513bb2f75
Brenton Farmer <brent.farmer@wagglebee.net>
2024-12-10 10:30:16 -0800

    fec unused field test

--- test/Hyperbee.Expressions.Tests/FastExpressionCompilerTests.cs
+++ test/Hyperbee.Expressions.Tests/FastExpressionCompilerTests.cs
@@ -1,4 +1,5 @@
-﻿using Hyperbee.Expressions.Tests.TestSupport;
+﻿using System.Linq.Expressions;
+using Hyperbee.Expressions.Tests.TestSupport;
 using static System.Linq.Expressions.Expression;
 
 namespace Hyperbee.Expressions.Tests;
@@ -6,13 +7,48 @@ namespace Hyperbee.Expressions.Tests;
 [TestClass]
 public class FastExpressionCompilerTests
 {
+    public class TestClass
+    {
+        public int Result0 = 42;
+        public int Result1;
+    }
 
     [DataTestMethod]
     [DataRow( CompilerType.Fast )]
     [DataRow( CompilerType.System )]
-    public void Compile_ShouldSucceed_WithRefParameter( CompilerType compiler )
+    public void Compile_ShouldSucceed_WithInnerNestedRefParameter( CompilerType compiler )
     {
         // TODO: FEC throws `System.InvalidProgramException: Common Language Runtime detected an invalid program.`
+
+        var testClassParameter = Parameter( typeof(TestClass), "testClass" );
+
+        var nestedLambda = Lambda(
+            Block(
+                Field( testClassParameter, nameof(TestClass.Result0) ), // Unused
+                Assign(
+                    Field( testClassParameter, nameof(TestClass.Result1) ),
+                    Field( testClassParameter, nameof(TestClass.Result0) )
+                )
+            ),
+            testClassParameter
+        );
+
+        var block = Block(
+            [testClassParameter],
+            Assign(
+                testClassParameter,
+                New( typeof(TestClass) )
+            ),
+            Invoke( nestedLambda, testClassParameter ),
+            Field( testClassParameter, nameof(TestClass.Result1) )
+        );
+
+        var lambda = Lambda<Func<int>>( block );
+        var compiledLambda = lambda.Compile( compiler );
+
+        var result = compiledLambda();
+
+        Assert.AreEqual( 42, result );
     }
 
     [DataTestMethod]
@@ -21,6 +57,31 @@ public class FastExpressionCompilerTests
     public void Compile_ShouldSucceed_WithUnusedValue( CompilerType compiler )
     {
         // TODO: FEC throws `System.InvalidProgramException: Common Language Runtime detected an invalid program.`
+
+        var testClassParameter = Parameter( typeof(TestClass), "testClass" );
+
+        var block = Block(
+            [testClassParameter],
+            Assign(
+                testClassParameter,
+                New( typeof(TestClass) )
+            ),
+            Block(
+                Field( testClassParameter, nameof(TestClass.Result0) ), // Unused
+                Assign(
+                    Field( testClassParameter, nameof(TestClass.Result1) ),
+                    Field( testClassParameter, nameof(TestClass.Result0) )
+                )
+            ),
+            Field( testClassParameter, nameof(TestClass.Result1) )
+        );
+
+        var lambda = Lambda<Func<int>>( block );
+        var compiledLambda = lambda.Compile( compiler );
+
+        var result = compiledLambda();
+
+        Assert.AreEqual( 42, result );
     }
 
     [DataTestMethod]
