2caf285ae3d51cf4c49acc713fe1f97b3eddb027
Matt Edwards <matthew.edwards@wagglebee.net>
2025-04-02 12:18:50 -0400

    Add Delegate based service provider Compile

--- src/Hyperbee.Expressions/CompileExpressionExtensions.cs
+++ src/Hyperbee.Expressions/CompileExpressionExtensions.cs
@@ -18,6 +18,20 @@ public static partial class ExpressionExtensions
             : throw new InvalidOperationException( "Failed to compile expression." );
     }
 
+    public static Delegate Compile(
+        this LambdaExpression expression,
+        IServiceProvider serviceProvider,
+        bool preferInterpretation = false )
+    {
+        ArgumentNullException.ThrowIfNull( serviceProvider, nameof( serviceProvider ) );
+
+        var setter = new ServiceSetter( serviceProvider );
+
+        return setter.Visit( expression ) is LambdaExpression replacedExpression
+            ? replacedExpression.Compile( preferInterpretation )
+            : throw new InvalidOperationException( "Failed to compile expression." );
+    }
+
     private class ServiceSetter( IServiceProvider serviceProvider ) : ExpressionVisitor
     {
         protected override Expression VisitExtension( Expression node )
